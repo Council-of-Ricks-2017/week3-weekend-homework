8522fd3a50b91b2be3253d0ebf43911ab6f2d19b
Shaun <seyyes2000@hotmail.com>
2017-02-05 07:01:02 -0500

    attempt to implement player/dealer tracking

--- blackjack.py
+++ blackjack.py
@@ -27,14 +27,21 @@ class Deck:
 	def __init__ (self):
 		self.deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 		self.player = Player()
+		self.dealer = Dealer()
 		self.card = None
+		# self.turn_count
 
 	# How do we return rand_card to dealer_cards and player_cards?
 	def rand_card(self):
-		#Need to put if statement in here determinining if Dealer or Player is playing
+		# if len(self.player.cards) in [0, ]
 		self.card = random.choice(self.deck)
-		self.player.cards.append(self.card)
-		print ("\n{}'s Cards: ".format(self.player.name), self.player.cards)
+
+		if self.player.player_turn == True:
+			self.player.cards.append(self.card)
+		else:
+			self.dealer.cards.append(self.card)
+
+		# print (type(len(self.player.cards)))
 		# print (self.card)
 
 
@@ -49,23 +56,30 @@ class Dealer:
 	def __init__(self):
 		self.dealer_total = 0
 		self.hit = True #dealer must hit if/until his total is 17 or more
-		self.card
+		self.card = None
 		self.cards = []
 
 
 class Turn:
 	#turn object should give player options/manage the turn
 	#report to game obj, update player obj.
+	# turn_count
+	#If turns = 1, 3, 5+.. then = player; else 2, 4, after player 5+ turns = dealer
+	#Maybe turn should count turns to track player/dealer turns
 	def __init__(self, player):
 		self.deck = Deck()
 		self.player = Player()
 		self.dealer = Dealer()
 		self.rand_card()
+		# turn_count += 1
 		self.display_cards()
 		#2 methods below should be part of display_cards() method
 			# self.calc_total()
 			# self.display_total()
-		self.hit_or_stay()
+
+		#Hit or stay should only run after 4 cards dealt
+		# self.hit_or_stay()
+
 		#outcome/compare to see if bust
 		#dealer hit
 		#output
@@ -73,19 +87,26 @@ class Turn:
 	def display_cards(self):
 		#graphical display of cards, figuring out value and giving to user
 		card = deck.rand_card()
-		player.get_card(card) #get_card should append the card to the self.cards
+		# player.get_card(card) #get_card should append the card to the self.cards
 		total = self.calc_total()
+		print ("\n{}'s Cards: ".format(self.player.name), self.player.cards)
 		print(player.cards, total)
-		self.hit_or_stay()
+		#keep this here or in __init__?, need if function to trigger
+		if player_turn == True:
+			self.hit_or_stay()
 
 	def hit_or_stay(self):
 		self.hit = input("Your total is {}, would you like to hit or stay?".format(self.player_total))
 		if self.hit == "hit":
-			return True
+			self.rand_card()
+			player_turn = True
+		else:
+			player_turn = False
 
 	def calc_total(self):
 		pass
 		#takes player cards and give it back to player
+		#updata_player_total is in the player class, should update total be one method use by both dealer and player?
 
 
 class Game:
@@ -99,7 +120,7 @@ class Game:
 
 	def set_up(self):
 		# self.player = Player()
-		#self.turn()
+		#self.turn() ## this should be first in the set_up method
 		#dealer has to play here
 		self.compare()
 		self.results()
