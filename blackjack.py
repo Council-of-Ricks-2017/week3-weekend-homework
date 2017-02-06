8afc7706750c68b0cd0897ad6ccbcd37acdd143c
Shaun <seyyes2000@hotmail.com>
2017-02-06 09:45:08 -0500

    rearrange code; still not working

--- blackjack.py
+++ blackjack.py
@@ -11,40 +11,36 @@ class Player:
 		self.player_total = 0
 		self.name = input("Enter your name: ")
 		self.hit = False
-		# self.card = None
 		self.cards = []
-
-	def get_card(self):
-		pass
 
 
 class Deck:
 	def __init__ (self):
 		self.deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
-		self.player = Player()
-		self.dealer = Dealer()
-		self.dealer_card = None
-		self.player_card = None
+		# return self.deck
+		# self.player = Player()
+		# self.dealer = Dealer()
+		# self.dealer_c/rd = None
 		# self.turn_count
 
 	# How do we return rand_card to dealer_cards and player_cards?
-	def rand_card(self):
+	# def player_rand_card(self):
+	# 	self.player_card = random.choice(self.deck)
+	# 	self.player.cards.append(self.player_card)
+	# 	return self.player.cards
 
-		self.player_card = random.choice(self.deck)
-		self.dealer_card = random.choice(self.deck)
+	# 	# if len(self.player.cards) in [0, 1]:
+	# 	# 	self.player.cards.append(self.player_card)
+	# 	# 	self.dealer.cards.append(self.dealer_card)
 
-		if len(self.player.cards) in [0, 1]:
-			self.player.cards.append(self.player_card)
-			self.dealer.cards.append(self.dealer_card)
+	# def dealer_rand_card(self):
+	# 	self.dealer_card = random.choice(self.deck)
+	# 	self.dealer.cards.append(self.dealer_card)
+	# 	return self.dealer.cards
 
-		# print ("\n{}'s Cards: ".format(self.dealer.name), self.dealer.cards)
-		# print ("{}'s Cards: ".format(self.player.name), self.player.cards, "\n")
-
-		# else:
-		# 	self.dealer.cards.append(self.card)
-
-		# print (type(len(self.player.cards)))
-		# print (self.card)
+		# if len(self.player.cards) in [0, 1]:
+		# 	self.player.cards.append(self.player_card)
+		# 	self.dealer.cards.append(self.dealer_card)
 
 
 class Dealer:
@@ -62,12 +58,16 @@ class Turn:
 	# turn_count
 	#If turns = 1, 3, 5+.. then = player; else 2, 4, after player 5+ turns = dealer
 	#Maybe turn should count turns to track player/dealer turns
-	def __init__(self, player):
-		self.deck = Deck()
+	def __init__(self):
+		self.hit = None
+		self.dealer_card = None
+		self.player_card = None
+		# self.deck = Deck()
 		self.player = Player()
 		self.dealer = Dealer()
-		self.game = Game()
-		self.deck.rand_card()
+		self.player_rand_card()
+		self.dealer_rand_card()
+		# self.set_up()
 		# turn_count += 1
 		self.display_cards()
 		#2 methods below should be part of display_cards() method
@@ -75,20 +75,36 @@ class Turn:
 			# self.display_total()
 
 		#Hit or stay should only run after 4 cards dealt
-		# self.hit_or_stay()
+		self.hit_or_stay()
+		# self.set_up()
 
 		#outcome/compare to see if bust
 		#dealer hit
 		#output
 
+	def player_rand_card(self):
+		self.player_card = random.choice(Deck())
+		self.player.cards.append(self.player_card)
+		return self.player.cards
+
+		# if len(self.player.cards) in [0, 1]:
+		# 	self.player.cards.append(self.player_card)
+		# 	self.dealer.cards.append(self.dealer_card)
+
+	def dealer_rand_card(self):
+		self.dealer_card = random.choice(self.deck)
+		self.dealer.cards.append(self.dealer_card)
+		return self.dealer.cards
+
+
 	def display_cards(self):
 		#graphical display of cards, figuring out value and giving to user
 		# card = deck.rand_card()
 		# player.get_card(card) #get_card should append the card to the self.cards
-		dealer_total = self.calc_dealer_total()
-		player_total = self.calc_player_total()
-		print ("\n{}'s Cards:\t".format(self.dealer.name), self.dealer.cards, "\t", dealer_total)
-		print ("\n{}'s Cards:\t".format(self.player.name), self.player.cards, "\t", player_total, "\n")
+		self.dealer.dealer_total = self.calc_dealer_total()
+		self.player.player_total = self.calc_player_total()
+		print ("\n{}'s Cards:\t".format(self.dealer.name), self.dealer.cards, "\t", self.dealer.dealer_total)
+		print ("\n{}'s Cards:\t".format(self.player.name), self.player.cards, "\t", self.player.player_total, "\n")
 		# print(player.cards, total)
 
 		#keep this here or in __init__?, need if function to trigger
@@ -96,7 +112,7 @@ class Turn:
 		# 	self.hit_or_stay()
 
 	def hit_or_stay(self):
-		self.hit = input("Your total is {}, would you like to hit or stay?".format(self.player_total))
+		self.hit = input("Your total is {}, would you like to hit or stay?".format(self.player.player_total))
 		if self.hit == "hit":
 			self.rand_card()
 			player_turn = True
@@ -127,17 +143,12 @@ class Turn:
 class Game:
 	def __init__(self):
 		#turn, dealer, and player should be in here
+		# self.deck = Deck() #Do i need this here?
 		self.turn = Turn()
-		self.dealer = Dealer()
-		self.deck = Deck()
-		self.player = Player()
-		# self.Turn()
 		self.set_up()
 
 	def set_up(self):
-		self.Turn() ## this should be first in the set_up method
-		#dealer has to play here
-		#Add turn methods here???????????
+		self.turn
 		self.compare()
 		self.results()
 
@@ -170,7 +181,7 @@ class Game:
 
 
 
-Game()
+g = Game()
 # dlr = Dealer()
 # plyr = Player()
 # dck = Deck()
